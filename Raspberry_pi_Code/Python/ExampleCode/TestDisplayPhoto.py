2dc546a80664b8bb5004960c544c9a010a12cedd
Jordan Fernandes <jfernan6@stevens.edu>
2023-01-26 15:32:38 -0500

    Added relative path to TestDisplayPhoto.py

--- Raspberry_pi_Code/Python/ExampleCode/TestDisplayPhoto.py
+++ Raspberry_pi_Code/Python/ExampleCode/TestDisplayPhoto.py
@@ -1,20 +1,21 @@
-#Import the required Libraries
+# Import the required Libraries
 from tkinter import *
-from PIL import Image,ImageTk
+from PIL import Image, ImageTk
 
-#Create an instance of tkinter frame
+# Create an instance of tkinter frame
 win = Tk()
 
-#Set the geometry of tkinter frame
-#win.geometry("750x400")
+# Set the geometry of tkinter frame
+# win.geometry("750x400")
 win.attributes('-fullscreen', True)
 wwid, whgt = win.winfo_screenwidth(), win.winfo_screenheight()
 
-#Create a canvas
-canvas= Canvas(win, width=wwid, height=whgt)
+# Create a canvas
+canvas = Canvas(win, width=wwid, height=whgt)
 canvas.pack()
 
-rawimg = Image.open("/home/pi/Desktop/Clearvoyance/Clearvoyance/Photos_Slides/piPhoto.jpg")
+# TODO: $HOME_DIR = pwd
+rawimg = Image.open("Photos_Slides/piPhoto.jpg")
 iwid, ihgt = rawimg.size
 
 ratio = min(wwid/iwid, whgt/ihgt)
@@ -22,10 +23,11 @@ iwid = int(iwid * ratio)
 ihgt = int(ihgt * ratio)
 rawimg = rawimg.resize((iwid, ihgt), Image.ANTIALIAS)
 
-#Load an image in the script
-img= ImageTk.PhotoImage(rawimg)
+# Load an image in the script
+img = ImageTk.PhotoImage(rawimg)
 
-#Add image to the Canvas Items
+# Add image to the Canvas Items
 canvas.create_image(wwid/2, whgt/2, image=img)
 
+# TODO: Close this loop somehow.
 win.mainloop()
