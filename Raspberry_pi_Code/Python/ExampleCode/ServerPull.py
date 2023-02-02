e834cae28144f057395598b85a030c6373480d7b
Jonathan Kubas <jkubas@stevens.edu>
2023-02-02 18:28:59 -0500

    Added ability for ServerPull script to write json to sample file, but will have to be changed in the future

--- Raspberry_pi_Code/Python/ExampleCode/ServerPull.py
+++ Raspberry_pi_Code/Python/ExampleCode/ServerPull.py
@@ -1,13 +1,6 @@
 import datetime
-
 import time
-
-from sys import argv
-
 import requests, json
-
-
-
 
 
 complete_url = "http://10.156.121.100/TestAlert.json"# The url is = http://api.openweathermap.org/data/2.5/weather?appid=eb737582e33e5f2a85cca9abb6e392e0&q=somerset,nj,us
@@ -21,9 +14,7 @@ response = requests.get(complete_url)
 x = response.json()
 
 
-
-#print(x)
-
-
+with open("sample.json", "w") as outfile:
+    json.dump(x, outfile)
 
 print(x["alert"][0]["Dec"])
