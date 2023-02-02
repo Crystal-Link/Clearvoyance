946351f205f758791b4040adf3eedf0afc0eabf7
mik1245 <michaelcompagnino@gmail.com>
2023-02-02 17:02:03 -0500

    ServerCode

---
+++ Raspberry_pi_Code/Python/ExampleCode/ServerPull.py
@@ -0,0 +1,29 @@
+import datetime
+
+import time
+
+from sys import argv
+
+import requests, json
+
+
+
+
+
+complete_url = "http://10.156.121.100/TestAlert.json"# The url is = http://api.openweathermap.org/data/2.5/weather?appid=eb737582e33e5f2a85cca9abb6e392e0&q=somerset,nj,us
+
+ 
+
+response = requests.get(complete_url) 
+
+
+
+x = response.json()
+
+
+
+#print(x)
+
+
+
+print(x["alert"][0]["Dec"])
