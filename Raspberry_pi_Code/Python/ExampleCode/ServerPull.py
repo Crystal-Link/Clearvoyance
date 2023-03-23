bdea0b13800f7b525c58d92ad80a52327def3abb
mik1245 <michaelcompagnino@gmail.com>
2023-03-23 15:11:14 -0400

    Added alert code

--- Raspberry_pi_Code/Python/ExampleCode/ServerPull.py
+++ Raspberry_pi_Code/Python/ExampleCode/ServerPull.py
@@ -6,13 +6,11 @@ import requests, json
 
 
 
-complete_url = "http://10.156.97.106//TestAlert.json"# The url is = http://api.openweathermap.org/data/2.5/weather?appid=eb737582e33e5f2a85cca9abb6e392e0&q=somerset,nj,us
+complete_url = "http://10.156.118.103//TestAlert.json"# The url is = http://api.openweathermap.org/data/2.5/weather?appid=eb737582e33e5f2a85cca9abb6e392e0&q=somerset,nj,us
 
 while True:
 
     response = requests.get(complete_url) 
-
-
 
     x = response.json()
 
